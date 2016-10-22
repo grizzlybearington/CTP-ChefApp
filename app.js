c8b1fdc6013534ce8053aecb9b6d53f2437d872e
jamaalg <jamaal1087@gmail.com>
2016-10-22 13:11:54 -0400

    added front end code to public folder

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
 
+
+app.use(express.static('./public'));
+
+app.get('/home', function(res,req){
+res.sendFile(__dirname + './public/html/index.html');
+});
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
 
 app.listen(8000, function () {
   console.log('App listening on port 8000!');
